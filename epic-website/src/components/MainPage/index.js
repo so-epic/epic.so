7de2c1f7ed82bf501a277adfd08c219af3cdfb70
Pedro da Matta <pmrm@cin.ufpe.br>
2020-08-14 23:37:45 -0300

    Get menu working

---
+++ epic-website/src/components/MainPage/index.js
@@ -0,0 +1,78 @@
+import React, { useRef, useState } from 'react';
+
+import { useMorph, easeOut } from 'react-morph';
+
+import '../../styles/app.css'
+
+import NavBar from '../NavBar'
+import MenuButton from '../MenuButton'
+import ContactButton from '../ContactButton'
+import Introduction from '../Introduction'
+import PabloVidal from '../PabloVidal'
+import TravisGiggy from '../TravisGiggy'
+import Methodology from '../Methodology'
+import StayInTouch from '../StayInTouch'
+import Services from '../Services'
+import Footer from '../Footer'
+import ContactModal from '../ContactModal'
+import MenuExpanded from '../MenuExpanded'
+
+import { HashLink as Link } from 'react-router-hash-link'
+
+
+
+const MainPage = () => {
+  const ref = useRef(null);
+  const [openContactModal, setOpenContactModal] = useState(false)
+  const [openMenuExpanded, setOpenMenuExpanded] = useState(false)
+  console.log(ref)
+
+  const contactMorph = useMorph({
+    spring: {
+      restDisplacementThreshold: 0.01,
+      overshootClamping: true,
+      damping: 5,
+      stiffness: 30,
+      mass: 1.4
+    },
+    easings: {
+      scaleY: easeOut,
+      scaleX: easeOut
+    }
+  });
+
+  const menuMorph = useMorph({
+    spring: {
+      restDisplacementThreshold: 0.01,
+      overshootClamping: true,
+      mass: 2,
+      damping: 5,
+      stiffness: 50
+    },
+    easings: {
+      scaleY: easeOut,
+      scaleX: easeOut
+    }
+  });
+
+
+  return (
+      <div className="flex flex-col overflow-hidden">
+        {openMenuExpanded && (<MenuExpanded menuMorph={menuMorph} setMenuExpanded={setOpenMenuExpanded} />)}
+        {openContactModal && (<ContactModal contactMorph={contactMorph} setContactModal={setOpenContactModal} />)}
+        <NavBar />
+        {!openMenuExpanded && (<MenuButton menuMorph={menuMorph} setMenuExpanded={setOpenMenuExpanded} />)}
+        {!openContactModal && (<ContactButton contactMorph={contactMorph} setContactModal = {setOpenContactModal} />)}
+        <Introduction />
+        <PabloVidal/>
+        <TravisGiggy id="travis" />
+        <Methodology />
+        <StayInTouch />
+        <Services />
+        <Footer />
+        <div id="oi"> deodkepokdep</div>
+      </div>    
+  );
+}
+
+export default MainPage;
--- epic-website/src/components/MenuList/index.js
+++ epic-website/src/components/MenuList/index.js
@@ -1,38 +1,53 @@
 import React from 'react';
 
+import { HashLink as Link } from 'react-router-hash-link'
+
+
 const MenuList = () => {
   return (
     <div class="absolute top-0 mt-24 w-screen rounded-md shadow-lg">
     <div class="rounded-md bg-transparent shadow-xs" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
-      <div class="py-1">
-        <a href="#" class="inline-block px-4 py-2 text-xl leading-5 text-white focus:outline-none focus:bg-gray-100 focus:text-gray-900" role="menuitem">01</a>
-        <a href="#" class="inline-block px-4 py-2 text-xl leading-5 text-white focus:outline-none focus:bg-gray-100 focus:text-gray-900" role="menuitem">Home</a>
-      </div>
+      <Link to="/#home">
+        <div class="py-1">
+          <a class="inline-block px-4 py-2 text-xl leading-5 text-white focus:outline-none focus:bg-gray-100 focus:text-gray-900" role="menuitem">01</a>
+          <a class="inline-block px-4 py-2 text-xl leading-5 text-white focus:outline-none focus:bg-gray-100 focus:text-gray-900" role="menuitem">Home</a>
+        </div>
+      </Link>
       <div class="border-t opacity-25 border-gray-100"></div>
-      <div class="py-1">
-        <a href="#" class="inline-block px-4 py-2 text-xl leading-5 text-white focus:outline-none focus:bg-gray-100 focus:text-gray-900" role="menuitem">02</a>
-        <a href="#" class="inline-block px-4 py-2 text-xl leading-5 text-white focus:outline-none focus:bg-gray-100 focus:text-gray-900" role="menuitem">About Us</a>
-      </div>
+      <Link to="/#travis">
+        <div class="py-1">
+          <a class="inline-block px-4 py-2 text-xl leading-5 text-white focus:outline-none focus:bg-gray-100 focus:text-gray-900" role="menuitem">02</a>
+          <a class="inline-block px-4 py-2 text-xl leading-5 text-white focus:outline-none focus:bg-gray-100 focus:text-gray-900" role="menuitem">About Us</a>
+        </div>
+      </Link>
       <div class="border-t opacity-25 border-gray-100"></div>
+      <Link to="/#benefits">
       <div class="py-1">
-        <a href="#" class="inline-block px-4 py-2 text-xl leading-5 text-white focus:outline-none focus:bg-gray-100 focus:text-gray-900" role="menuitem">03</a>
-        <a href="#" class="inline-block px-4 py-2 text-xl leading-5 text-white focus:outline-none focus:bg-gray-100 focus:text-gray-900" role="menuitem">Benefits</a>
+        <a class="inline-block px-4 py-2 text-xl leading-5 text-white focus:outline-none focus:bg-gray-100 focus:text-gray-900" role="menuitem">03</a>
+        <a class="inline-block px-4 py-2 text-xl leading-5 text-white focus:outline-none focus:bg-gray-100 focus:text-gray-900" role="menuitem">Benefits</a>
       </div>
+      </Link>
       <div class="border-t opacity-25 border-gray-100"></div>
+      <Link to="/#methology">
       <div class="py-1">
-        <a href="#" class="inline-block px-4 py-2 text-xl leading-5 text-white focus:outline-none focus:bg-gray-100 focus:text-gray-900" role="menuitem">04</a>
-        <a href="#" class="inline-block px-4 py-2 text-xl leading-5 text-white focus:outline-none focus:bg-gray-100 focus:text-gray-900" role="menuitem">Our methodology</a>
+        <a class="inline-block px-4 py-2 text-xl leading-5 text-white focus:outline-none focus:bg-gray-100 focus:text-gray-900" role="menuitem">04</a>
+        <a class="inline-block px-4 py-2 text-xl leading-5 text-white focus:outline-none focus:bg-gray-100 focus:text-gray-900" role="menuitem">Our methodology</a>
       </div>
+      </Link>
       <div class="border-t opacity-25 border-gray-100"></div>
+      <Link to="/blog">
       <div class="py-1">
-        <a href="#" class="inline-block px-4 py-2 text-xl leading-5 text-white focus:outline-none focus:bg-gray-100 focus:text-gray-900" role="menuitem">05</a>
-        <a href="#" class="inline-block px-4 py-2 text-xl leading-5 text-white focus:outline-none focus:bg-gray-100 focus:text-gray-900" role="menuitem">Blog</a>
+        <a class="inline-block px-4 py-2 text-xl leading-5 text-white focus:outline-none focus:bg-gray-100 focus:text-gray-900" role="menuitem">05</a>
+        <a class="inline-block px-4 py-2 text-xl leading-5 text-white focus:outline-none focus:bg-gray-100 focus:text-gray-900" role="menuitem">Blog</a>
       </div>
+      </Link>
       <div class="border-t opacity-25 border-gray-100"></div>
+      <Link to="/#stayInTouch">
       <div class="py-1">
-        <a href="#" class="inline-block px-4 py-2 text-xl leading-5 text-white focus:outline-none focus:bg-gray-100 focus:text-gray-900" role="menuitem">06</a>
-        <a href="#" class="inline-block px-4 py-2 text-xl leading-5 text-white focus:outline-none focus:bg-gray-100 focus:text-gray-900" role="menuitem">Contact Us</a>
+        <a class="inline-block px-4 py-2 text-xl leading-5 text-white focus:outline-none focus:bg-gray-100 focus:text-gray-900" role="menuitem">06</a>
+        <a class="inline-block px-4 py-2 text-xl leading-5 text-white focus:outline-none focus:bg-gray-100 focus:text-gray-900" role="menuitem">Contact Us</a>
       </div>
+      </Link>
       <div class="border-t opacity-25  border-gray-100"></div>
     </div>
   </div>
