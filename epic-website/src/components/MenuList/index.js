42466939af376432ffc011ee455a33d10968a9e7
Pedro da Matta <pmrm@cin.ufpe.br>
2020-08-14 23:39:19 -0300

    Small fix in menu animation

--- epic-website/src/components/MenuList/index.js
+++ epic-website/src/components/MenuList/index.js
@@ -7,42 +7,42 @@ const MenuList = () => {
   return (
     <div class="absolute top-0 mt-24 w-screen rounded-md shadow-lg">
     <div class="rounded-md bg-transparent shadow-xs" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
-      <Link to="/#home">
+      <Link smooth to="/#home">
         <div class="py-1">
           <a class="inline-block px-4 py-2 text-xl leading-5 text-white focus:outline-none focus:bg-gray-100 focus:text-gray-900" role="menuitem">01</a>
           <a class="inline-block px-4 py-2 text-xl leading-5 text-white focus:outline-none focus:bg-gray-100 focus:text-gray-900" role="menuitem">Home</a>
         </div>
       </Link>
       <div class="border-t opacity-25 border-gray-100"></div>
-      <Link to="/#travis">
+      <Link smooth to="/#travis">
         <div class="py-1">
           <a class="inline-block px-4 py-2 text-xl leading-5 text-white focus:outline-none focus:bg-gray-100 focus:text-gray-900" role="menuitem">02</a>
           <a class="inline-block px-4 py-2 text-xl leading-5 text-white focus:outline-none focus:bg-gray-100 focus:text-gray-900" role="menuitem">About Us</a>
         </div>
       </Link>
       <div class="border-t opacity-25 border-gray-100"></div>
-      <Link to="/#benefits">
+      <Link smooth to="/#benefits">
       <div class="py-1">
         <a class="inline-block px-4 py-2 text-xl leading-5 text-white focus:outline-none focus:bg-gray-100 focus:text-gray-900" role="menuitem">03</a>
         <a class="inline-block px-4 py-2 text-xl leading-5 text-white focus:outline-none focus:bg-gray-100 focus:text-gray-900" role="menuitem">Benefits</a>
       </div>
       </Link>
       <div class="border-t opacity-25 border-gray-100"></div>
-      <Link to="/#methology">
+      <Link smooth to="/#methology">
       <div class="py-1">
         <a class="inline-block px-4 py-2 text-xl leading-5 text-white focus:outline-none focus:bg-gray-100 focus:text-gray-900" role="menuitem">04</a>
         <a class="inline-block px-4 py-2 text-xl leading-5 text-white focus:outline-none focus:bg-gray-100 focus:text-gray-900" role="menuitem">Our methodology</a>
       </div>
       </Link>
       <div class="border-t opacity-25 border-gray-100"></div>
-      <Link to="/blog">
+      <Link smooth to="/blog">
       <div class="py-1">
         <a class="inline-block px-4 py-2 text-xl leading-5 text-white focus:outline-none focus:bg-gray-100 focus:text-gray-900" role="menuitem">05</a>
         <a class="inline-block px-4 py-2 text-xl leading-5 text-white focus:outline-none focus:bg-gray-100 focus:text-gray-900" role="menuitem">Blog</a>
       </div>
       </Link>
       <div class="border-t opacity-25 border-gray-100"></div>
-      <Link to="/#stayInTouch">
+      <Link smooth to="/#stayInTouch">
       <div class="py-1">
         <a class="inline-block px-4 py-2 text-xl leading-5 text-white focus:outline-none focus:bg-gray-100 focus:text-gray-900" role="menuitem">06</a>
         <a class="inline-block px-4 py-2 text-xl leading-5 text-white focus:outline-none focus:bg-gray-100 focus:text-gray-900" role="menuitem">Contact Us</a>
