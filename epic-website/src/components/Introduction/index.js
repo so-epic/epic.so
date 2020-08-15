7d99e83876cd9725ea29c82be70c551748c4e09f
Pedro da Matta <pmrm@cin.ufpe.br>
2020-08-14 21:27:56 -0300

    fix/animations Fix contact us and contact buttons animation and fonts

--- epic-website/src/components/Introduction/index.js
+++ epic-website/src/components/Introduction/index.js
@@ -12,7 +12,7 @@ const Introduction = () => {
         <h2 class="font-epicLogo font-bold mt-32 ml-8 text-epicTitle text-epicTitleBlue md:ml-16 md:text-personNameSize">epic</h2>
         <h1 class="font-beBetter font-bold text-beBetterSize text-beBetterBlue tracking-wider mt-8 ml-8 md:ml-16 md:text-beBetterMdSize">Be better.</h1>
         <p class="font-beBetter font-light text-descriptionSize text-beBetterBlue tracking-widest mt-4 ml-8 md:ml-16 md:text-buttonSize">Grow a healthy business focusing <br/> on the right customers</p>
-        <div class="hidden md:inline ml-16 mt-8">
+        <div class="hidden md:inline mt-8">
           <ContactButton/>
         </div>
       </div>
