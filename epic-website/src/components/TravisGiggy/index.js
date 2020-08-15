de1acadd3c4977d12beed9e7be4ed273d511cbee
Pedro da Matta <pmrm@cin.ufpe.br>
2020-08-14 21:46:46 -0300

    fix travis giggy responsiveness

--- epic-website/src/components/TravisGiggy/index.js
+++ epic-website/src/components/TravisGiggy/index.js
@@ -9,14 +9,14 @@ const TravisGiggy = () => {
     <div class="flex flex-col md:flex-row md:justify-around items-center h-full mt-4 mb-32">
       <div class="md:flex md:flex-col md:self-start md:order-1 md:-mt-8 md:ml-32 md:px-16">
         <img class="object-contain -ml-24 md:-ml-24 inset-16 md:w-56" src={CurvedLines} alt="" />
-        <img class="z-10 -mt-40 md:mt-0 md:-mt-32 md:w-126" src={TravisGiggyImage} alt="PabloVidal" />
+        <img class="z-10 -mt-40 md:mt-0 md:-mt-32 md:w-126 md:min-w-400px" src={TravisGiggyImage} alt="TravisGiggy" />
       </div>
-      <img class="absolute hidden md:inline self-end right-0 mt-24" src={ProfileDecoration2} alt="PabloVidal" />
-      <div class="flex flex-col md:min-w-1/2 md:px-64 md:order-2">
-        <h1 class="font-beBetter ml-8 font-medium text-personNameSize md:text-personNameMdSize text-beBetterBlue leading-tight mt-8">
+      <img class="absolute hidden md:inline self-end right-0 mt-24" src={ProfileDecoration2} alt="Decoration" />
+      <div class="flex flex-col md:min-w-1/2 md:order-2">
+        <h1 class="font-beBetter ml-16 md:ml-64 font-medium text-personNameSize md:text-personNameMdSize text-beBetterBlue leading-tight mt-8">
           Travis Giggy
         </h1>
-        <p class="font-beBetter ml-8 md:max-w-md font-regular text-descriptionSize text-personDescriptionColor tracking-widest leading-7 mt-4 breaking-normal">
+        <p class="font-beBetter ml-8 md:ml-64 md:max-w-md font-regular text-descriptionSize text-personDescriptionColor tracking-widest leading-7 mt-4 breaking-normal">
           Travis Giggy is a serial enterpreneur in
           Silicon Valley with more than 20 years
           leading product and technology teams.
@@ -27,10 +27,10 @@ const TravisGiggy = () => {
           development, product management, big 
           data analysis, and financial engineering.
         </p>
-        <h2 class="font-beBetter ml-8 font-medium text-personJobSize text-personDescriptionColor mt-4">
+        <h2 class="font-beBetter ml-8 md:ml-64 font-medium text-personJobSize text-personDescriptionColor mt-4">
           Growth Expert
         </h2>
-      <button class="mx-28 md:ml-8 bg-buttonColor w-40 h-6 text-white text-buttonSize font-beBetter font-medium rounded-xlg mt-8 mb-16">
+      <button class="mx-28 md:ml-64 bg-buttonColor w-40 h-6 text-white text-buttonSize font-beBetter font-medium rounded-xlg mt-8 mb-16">
         See more
       </button>
       </div>
