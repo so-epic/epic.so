69b36628b029a54ae65d674b1283b40b2677e786
Pedro da Matta <pmrm@cin.ufpe.br>
2020-08-14 22:32:43 -0300

    Small fixes on responsiveness

--- epic-website/src/components/Introduction/index.js
+++ epic-website/src/components/Introduction/index.js
@@ -9,7 +9,7 @@ const Introduction = () => {
   return (
     <div class="flex flex-col h-screen mt-16 introduction-div overflow-hidden">
       <div class="flex flex-col self-start">
-        <h2 class="font-epicLogo font-bold mt-32 ml-8 text-epicTitle text-epicTitleBlue md:ml-16 md:text-personNameSize">epic</h2>
+        <h2 class="font-epicLogo font-bold mt-32 ml-8 text-epicTitle text-epicTitleBlue md:ml-16 md:text-personNameSize epic-title-div">epic</h2>
         <h1 class="font-beBetter font-bold text-beBetterSize text-beBetterBlue tracking-wider mt-8 ml-8 md:ml-16 md:text-beBetterMdSize">Be better.</h1>
         <p class="font-beBetter font-light text-descriptionSize text-beBetterBlue tracking-widest mt-4 ml-8 md:ml-16 md:text-buttonSize">Grow a healthy business focusing <br/> on the right customers</p>
         <div class="hidden md:inline mt-8">
@@ -19,7 +19,7 @@ const Introduction = () => {
       <div class="hidden md:inline mt-16">
         <LinesIntroduction />
       </div>
-      <div class="absolute md:inline hidden -mt-32">
+      <div class="absolute md:inline hidden div-svg-video">
         <svg class="absolute" xmlns="http://www.w3.org/2000/svg">
           <clipPath id="clip" transform="translate(77 0)">
             <path
