e962d6da19219ff30081e2b1bad5fce4994866cc
Pedro da Matta <pmrm@cin.ufpe.br>
2020-08-17 18:43:07 -0300

    Fix fingerprint logo resolution

--- epic-website/src/components/NavBar/index.js
+++ epic-website/src/components/NavBar/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import Fingerprint from './Fingerprint'
+
+import fingerprintest from '../../assets/images/fingerprintest.png'
 
 import { HashLink as Link } from 'react-router-hash-link'
 
@@ -11,7 +12,7 @@ const NavBar = () => {
     <nav className="hidden md:flex fixed w-full items-center justify-between bg-white pt-8 z-80">
       <div className="flex flex-col items-center text-white">
         <Link className="-mt-8 ml-8 z-20" to="/#home"><span className="font-epicLogo font-bold text-epicTitle text-beBetterBlue md:text-personNameSize z-10 ml-24">epic</span></Link>
-        <Fingerprint />
+        <img className="absolute w-24 -mt-16" src={fingerprintest}></img>
       </div>
       <div className="w-full block md:flex md:items-center md:w-auto md:mr-16">
         <div className="text-md space-x-8">
