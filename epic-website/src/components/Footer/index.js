daf2175fa7cd5b5bc4add22ac29b851b7632e624
Pedro da Matta <pmrm@cin.ufpe.br>
2020-08-17 19:57:22 -0300

    Fixes footer size on mobile landscape

--- epic-website/src/components/Footer/index.js
+++ epic-website/src/components/Footer/index.js
@@ -8,7 +8,7 @@ import './footer.css'
 const Footer = () => {
   return(
     <div class="flex mt-32 justify-center items-center">
-      <img alt="" class="md:inline hidden w-10/12" src={FooterWebBackground} />
+      <img alt="" class="md:inline hidden md:w-full md:ml-4 lg:ml-0 lg:w-10/12" src={FooterWebBackground} />
       <img alt="" class="md:hidden w-11/12" src={FooterMobileBackground} />
       <h2 class="font-epicLogo font-bold absolute self-start mt-40 z-50 text-4xl text-black md:-ml-32 md:mt-24 md:text-5xl epic-title-div">epic</h2>
     </div>
