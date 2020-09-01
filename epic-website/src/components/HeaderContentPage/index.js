145d2375126c1287c54abda2b4137de9f6133865
Travis Giggy <travis@giggy.com>
2020-09-01 10:48:43 -0700

    adding articles to blog page

--- epic-website/src/components/HeaderContentPage/index.js
+++ epic-website/src/components/HeaderContentPage/index.js
@@ -11,6 +11,9 @@ import './headerContentPage.css'
 
 import CurvedLines from '../../assets/images/curved-lines.svg'
 import ImgWhyThisWhyNow from '../../assets/images/green-arrows.jpg'
+import ImgCohort from '../../assets/images/111.10.30.02-cohort-expand.gif'
+import ImgCOVID from '../../assets/images/img-covid-tree.gif'
+
 import ProfileDecoration2 from '../../assets/images/profile-decoration2.svg'
 
 const HeaderContentPage = () => {
@@ -58,11 +61,71 @@ const HeaderContentPage = () => {
           </p>
         </div>
 
-        <div id="post" class="flex flex-col lg:flex-row md:items-center h-full mt-16 md:space-x-16 md:mb-32 md:max-w-80%">
+
+
+        {/* WHAT IS A COHORT AND WHY SHOULD I CARE */}
+        <div id="post" class="flex flex-col lg:flex-row md:items-center h-full mt-16 md:space-x-16 mb-32 md:max-w-80%">
+
+          <a target="_blank" rel="noopener noreferrer" href="https://medium.com/epiclabs/what-is-a-cohort-and-why-should-i-care-3bc689aca23b">
+            <div class="md:flex md:flex-col md:order-1 md:-mt-8">
+              <img class="object-contain -ml-24 md:-ml-24 inset-16 md:w-56" src={CurvedLines} alt="" />
+              <img class="z-10 -mt-40 md:mt-0 md:-mt-32 md:min-w-600px" src={ImgCohort} alt="Cohort Analysis" />
+            </div>
+          </a>
+
+          <img class="absolute hidden md:inline self-end right-0 mt-24" src={ProfileDecoration2} alt="Decoration" />
+
+          <div class="flex flex-col md:min-w-1/2 md:order-2 lg:m-2">
+            <h1 class="font-beBetter font-medium text-personNameSize md:text-5xl text-beBetterBlue leading-tight breaking-normal mt-8">
+              What is a cohort and why should I care?
+            </h1>
+
+            <p class="font-beBetter md:ml-0 font-regular text-descriptionSize text-personDescriptionColor tracking-widest leading-7 mt-4 breaking-normal">
+              <i>Sep 1, 2020</i> - The manager in a business is like the head chef in a restaurant, and skill with Cohort Analysis helps you cook up some profitable dishes.
+            </p>
+
+            <button href="link" class="bg-buttonColor w-40 h-6 text-white text-buttonSize self-center md:self-start font-beBetter font-medium rounded-xlg mt-8 mb-16 z-50">
+              <a target="_blank" rel="noopener noreferrer" href="https://medium.com/epiclabs/what-is-a-cohort-and-why-should-i-care-3bc689aca23b">Read more</a>
+            </button>
+
+          </div>
+
+        </div>
+
+
+        {/* WHAT CAN COVID TEACH US ABOUT BUSINESS */}
+        <div id="post" class="flex flex-col lg:flex-row md:items-center h-full md:space-x-16 mb-32 md:max-w-80%">
+
+          <a target="_blank" rel="noopener noreferrer" href="https://medium.com/epiclabs/what-can-covid-teach-us-about-business-c6fd363e804c">
+            <div class="md:flex md:flex-col md:order-1 md:-mt-8">
+              <img class="z-10 -mt-40 md:mt-0 md:-mt-32 md:min-w-600px" src={ImgCOVID} alt="Grow your business the natural way" />
+            </div>
+          </a>
+
+          <div class="flex flex-col md:min-w-1/2 md:order-2 lg:m-2">
+            <h1 class="font-beBetter font-medium text-personNameSize md:text-5xl text-beBetterBlue leading-tight breaking-normal mt-8">
+              What can COVID teach us about business?
+            </h1>
+
+            <p class="font-beBetter md:ml-0 font-regular text-descriptionSize text-personDescriptionColor tracking-widest leading-7 mt-4 breaking-normal">
+              <i>Aug 27, 2020</i> - In the business world, a pandemic is interesting as an example of exponential growth and virality. We often use the term “viral marketing” or “going viral”, but rarely take the time to think about what it means. In the case of COVID-19 “virality”, there are interesting corollaries for business growth.
+            </p>
+
+            <button href="link" class="bg-buttonColor w-40 h-6 text-white text-buttonSize self-center md:self-start font-beBetter font-medium rounded-xlg mt-8 mb-16 z-50">
+              <a target="_blank" rel="noopener noreferrer" href="https://medium.com/epiclabs/what-can-covid-teach-us-about-business-c6fd363e804c">Read more</a>
+            </button>
+
+          </div>
+
+        </div>
+
+
+
+
+        <div id="post" class="flex flex-col lg:flex-row md:items-center h-full mt-16 md:space-x-16 mb-32 md:max-w-80%">
 
           <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/pulse/why-now-travis-giggy/">
             <div class="md:flex md:flex-col md:order-1 md:-mt-8">
-              <img class="object-contain -ml-24 md:-ml-24 inset-16 md:w-56" src={CurvedLines} alt="" />
               <img class="z-10 -mt-40 md:mt-0 md:-mt-32 md:min-w-600px" src={ImgWhyThisWhyNow} alt="ImagePost" />
             </div>
           </a>
